89d87b079a5104c876d679d7ed5b227bc2c77cb5
asierpl <asierpl210395@gmail.com>
2024-06-12 19:04:32 +0200

    eliminar libro

--- src/pages/Libros.jsx
+++ src/pages/Libros.jsx
@@ -51,6 +51,24 @@ export const Libros = () => {
             .then( res => res.json() )
             .then( data => setLibros(data))
     }
+
+    const deleteHandler = async (id) => {
+        
+        let options = {
+            method : 'delete',
+            headers : { "Content-type" : "application/json" }
+        }
+
+        await fetch(`${VITE_URL_API}/libros/${id}` , options )
+        
+        const res = await fetch(`${VITE_URL_API}/libros`)
+
+        const data = await res.json()
+
+        setLibros(data)
+    }
+    
+
     return(
         <>
         <NavLink to={`/inicio`} className="HeaderNav-a">Inicio</NavLink> 
@@ -63,6 +81,7 @@ export const Libros = () => {
                     <p>{item.nombre}</p>
                     <p>{item.autor}</p>
                     <p>{item.fecha}</p>
+                    <button onClick={()=> deleteHandler(item._id)} >Eliminar</button>
                 </div>
             ))
         ) : (
