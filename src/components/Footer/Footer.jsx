85ec99e13ee5e36f3182986d7e34041d8f8d2a83
asierpl <asierpl210395@gmail.com>
2024-06-20 12:53:18 +0200

    footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+//TERMINAL INSTALAR --> npm install @fortawesome/fontawesome-svg-core @fortawesome/free-brands-svg-icons @fortawesome/react-fontawesome
+
 // Añade todos los iconos de marcas (brand icons) a la librería
 library.add(fab);
 
@@ -13,11 +15,6 @@ export const Footer = () => {
     return(
         <>
         <div className="Footer">
-            <div className="Politicas">
-                <p className="Politica">Política de cookies</p>
-                <p className="Politica">Política de privacidad</p>
-                <p className="Politica">Información legal</p>
-            </div>
             <div className="Redes">
                 <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="Red">
                     <FontAwesomeIcon icon={['fab', 'facebook']} /> Facebook
@@ -29,12 +26,20 @@ export const Footer = () => {
                     <FontAwesomeIcon icon={['fab', 'instagram']} /> Instagram
                 </a>
             </div>
+            <div className="Politicas">
+                <p className="Politica">Política de cookies</p>
+                <p className="Politica">Política de privacidad</p>
+                <p className="Politica">Información legal</p>
+            </div>
             <div className="Paginas">
                 <a href="https://www.goodreads.com/"  target="_blank" rel="noopener noreferrer" className="Pagina">Goodreads</a>
                 <a href="https://entrelectores.com/"  target="_blank" rel="noopener noreferrer" className="Pagina">EntreLectores</a>
                 <a href="https://www.lecturalia.com/" target="_blank" rel="noopener noreferrer" className="Pagina">Lecturalia</a>
                 <a href="https://www.wattpad.com/"    target="_blank" rel="noopener noreferrer" className="Pagina">Wattpad</a>
                 <a href="https://www.librote.com/"    target="_blank" rel="noopener noreferrer" className="Pagina">Librote</a>
+            </div>
+            <div className="Luma-footer">
+                <h4 className='Luma-h4'>LUMA</h4>
             </div>
         </div>
         </>
